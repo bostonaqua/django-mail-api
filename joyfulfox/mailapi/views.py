ec8df0a5efe8f97d0f959d54ae97c064105cc53b
Aleksander Kiyashchenko <kiyaschenko@gmail.com>
2020-01-28 03:10:28 +0200

    message model

--- joyfulfox/mailapi/views.py
+++ joyfulfox/mailapi/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.http import HttpResponseServerError
+from django.shortcuts import HttpResponse
+import json
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("*\-(^_^)-/*")
+
+
+def save_events_json(request):
+    if request.is_ajax():
+        if request.method == 'POST':
+            json_data = json.loads(request.body)
+            try:
+                host = json_data['host']
+                port = json_data['port']
+                mail_from = json_data['from']
+                mail_to = json_data['to']
+                subject = json_data['subject']
+                message = json_data['message']
+            except KeyError:
+                HttpResponseServerError("Malformed data!")
+    return HttpResponse("OK")
