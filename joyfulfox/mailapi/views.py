4dfa5c9201952f321cb376e6ce37e9de3a84b308
Aleksander Kiyashchenko <kiyaschenko@gmail.com>
2020-01-30 20:33:21 +0200

    remove ajax

--- joyfulfox/mailapi/views.py
+++ joyfulfox/mailapi/views.py
@@ -13,29 +13,28 @@ def index(request):
 
 @csrf_exempt
 def send_mail_view(request):
-    if request.is_ajax():
-        if request.method == 'POST':
-            json_data = json.loads(request.body.decode('utf-8'))
-            try:
-                host = json_data['host']
-                port = json_data['port']
-                mail_from = json_data['from']
-                mail_to = json_data['to']
-                subject = json_data['subject']
-                message = json_data['message']
-                username = json_data['username']
-                password = json_data['password']
-                send_mail(
-                    "{} - {}".format(subject, mail_from),
-                    "{}\n\n---\nFrom: {}".format(message, mail_from),
-                    "{}".format(username),
-                    ['{}'.format(mail_to)],
-                    auth_user=username,
-                    auth_password=password,
-                    connection=EmailBackend(host=host, port=port, username=username, password=password)
-                )
-            except KeyError:
-                return HttpResponseServerError("Malformed data!")
-            return HttpResponse("OK")
+    if request.method == 'POST':
+        json_data = json.loads(request.body.decode('utf-8'))
+        try:
+            host = json_data['host']
+            port = json_data['port']
+            mail_from = json_data['from']
+            mail_to = json_data['to']
+            subject = json_data['subject']
+            message = json_data['message']
+            username = json_data['username']
+            password = json_data['password']
+            send_mail(
+                "{} - {}".format(subject, mail_from),
+                "{}\n\n---\nFrom: {}".format(message, mail_from),
+                "{}".format(username),
+                ['{}'.format(mail_to)],
+                auth_user=username,
+                auth_password=password,
+                connection=EmailBackend(host=host, port=port, username=username, password=password)
+            )
+        except KeyError:
+            return HttpResponseServerError("Malformed data!")
+        return HttpResponse("OK")
     else:
         return HttpResponseServerError("Service unavailable")
